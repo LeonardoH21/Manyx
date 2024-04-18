3dc4e367f3c388abd7ad928cac6756e93e18d45a
LeonardoH21 <127771044+LeonardoH21@users.noreply.github.com>
2024-04-17 22:47:25 -0300

    Add files via upload

---
+++ frontend/src/components/Grid.js
@@ -0,0 +1,102 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+import { FaTrash, FaEdit } from "react-icons/fa";
+import { toast } from "react-toastify";
+
+const Table = styled.table`
+  width: 100%;
+  background-color: #fff;
+  padding: 20px;
+  box-shadow: 0px 0px 5px #ccc;
+  border-radius: 5px;
+  max-width: 1120px;
+  margin: 20px auto;
+  word-break: break-all;
+`;
+
+export const Thead = styled.thead``;
+
+export const Tbody = styled.tbody``;
+
+export const Tr = styled.tr``;
+
+export const Th = styled.th`
+  text-align: start;
+  border-bottom: inset;
+  padding-bottom: 5px;
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+export const Td = styled.td`
+  padding-top: 15px;
+  text-align: ${(props) => (props.alignCenter ? "center" : "start")};
+  width: ${(props) => (props.width ? props.width : "auto")};
+
+  @media (max-width: 500px) {
+    ${(props) => props.onlyWeb && "display: none"}
+  }
+`;
+
+const Grid = ({ users, setUsers, setOnEdit }) => {
+  const handleEdit = (item) => {
+    setOnEdit(item);
+  };
+
+  const formatDate = (dateString) => {
+    const date = new Date(dateString);
+    return date.toLocaleDateString(); // Ajusta para exibir apenas a data
+  };
+
+  const handleDelete = async (id) => {
+    await axios
+      .delete("http://localhost:8800/" + id)
+      .then(({ data }) => {
+        const newArray = users.filter((user) => user.id !== id);
+
+        setUsers(newArray);
+        toast.success(data);
+      })
+      .catch(({ data }) => toast.error(data));
+
+    setOnEdit(null);
+  };
+
+  return (
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Nome</Th>
+          <Th>Email</Th>
+          <Th>Sessão</Th>
+          <Th>Horário</Th>
+          <Th>Data de Agendamento</Th>
+          <Th></Th>
+          <Th></Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {users.map((item, i) => (
+          <Tr key={i}>
+            <Td width="15%">{item.nome}</Td>
+            <Td width="30%">{item.email}</Td>
+            <Td width="15%">{item.sessao}</Td>
+            <Td width="10%">{item.horario}</Td>
+            <Td width="30%">{formatDate(item.data_agendamento)}</Td>
+            <Td alignCenter width="5%">
+              <FaEdit onClick={() => handleEdit(item)} />
+            </Td>
+            <Td alignCenter width="5%">
+              <FaTrash onClick={() => handleDelete(item.id)} />
+            </Td>
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  );
+};
+
+export default Grid;
